bd22cf382a2c72dcac512795e4b327104451dc8f
Alberto Asuero <alberto.asuero@geographica.gs>
2013-12-10 11:11:00 +0100

    Big command

--- src/tilemaker/tilemaker.py
+++ src/tilemaker/tilemaker.py
@@ -10,11 +10,11 @@ if len(sys.argv) != 3:
 src = sys.argv[1]
 target = sys.argv[2]
 ZOOM_START = 3
-ZOOM_END = 20
+ZOOM_END = 17
 SYS_REF = "EPSG:23030"
-remove_tmp = False
+remove_tmp = True
 
-GDAL_BIN_DIRECTORY = "/usr/local/gis/gdal-1.10-1/bin/"
+GDAL_BIN_DIRECTORY = ""
 
 #list of supported extensions
 suportedExtensions = [".sid"]
@@ -62,14 +62,21 @@ os.makedirs(tmp)
 
 tiffFile = os.path.join(tmp,"mosaic.tiff")
 print "Creating big mosaic"
-cmd = "%s%s%s %s" % (GDAL_BIN_DIRECTORY, "gdalwarp -of GTiff -dstalpha -r cubic "," ".join(srcFiles),tiffFile)
-os.system(cmd)
+cmd = "%s%s%s %s" % (GDAL_BIN_DIRECTORY, "#!/bin/bash\ngdalwarp -of GTiff -dstalpha -r cubic "," ".join(srcFiles),tiffFile)
+script_path = os.path.join(tmp,"gdalwarp_script")
+text_file = open(script_path, "w")
+text_file.write(cmd)
+text_file.close()
+
+os.system("chmod +x " + script_path )
+os.system( script_path)
 print "Mosaic created successfully"
+
 
 mosaic_time = time.time() - start_time
 
 print "Creating tiles"
-cmd =  "python %sgdal2tiles.py -r cubic -s %s -z %d-%d %s %s" % (GDAL_BIN_DIRECTORY,SYS_REF,ZOOM_START,ZOOM_END,tiffFile,target)
+cmd =  "%sgdal2tiles.py -r cubic -s %s -z %d-%d %s %s" % (GDAL_BIN_DIRECTORY,SYS_REF,ZOOM_START,ZOOM_END,tiffFile,target)
 os.system(cmd)
 print "Tiles created successfully"
 
