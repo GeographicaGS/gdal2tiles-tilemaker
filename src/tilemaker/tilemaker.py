009f56668478d1b37c492f3066ede8d229837b99
Alberto Asuero <alberto.asuero@geographica.gs>
2013-12-16 08:48:56 +0100

    Remove output XML

--- src/tilemaker/tilemaker.py
+++ src/tilemaker/tilemaker.py
@@ -10,9 +10,10 @@ if len(sys.argv) != 3:
 src = sys.argv[1]
 target = sys.argv[2]
 ZOOM_START = 3
-ZOOM_END = 16
+ZOOM_END = 17
 SYS_REF = "EPSG:23030"
 remove_tmp = True
+remove_output_XML = True
 
 GDAL_BIN_DIRECTORY = ""
 
@@ -61,6 +62,8 @@ os.makedirs(tmp)
 
 tiffFile = os.path.join(tmp,"mosaic.vrt")
 print "Creating big mosaic"
+sys.stdout.flush()
+
 cmd = "%s%s%s %s" % (GDAL_BIN_DIRECTORY, "#!/bin/bash\ngdalwarp -of VRT -dstalpha -r cubic "," ".join(srcFiles),tiffFile)
 #gdalbuildvrt doq_index.vrt
 cmd = "%s %s %s" % ("#!/bin/bash\ngdalbuildvrt -addalpha",tiffFile," ".join(srcFiles))
@@ -72,19 +75,38 @@ text_file.close()
 os.system("chmod +x " + script_path )
 os.system( script_path)
 print "Mosaic created successfully"
+sys.stdout.flush()
+
 
 mosaic_time = time.time() - start_time
 
 print "Creating tiles"
+sys.stdout.flush()
 cmd =  "%sgdal2tiles.py -r cubic -s %s -z %d-%d %s %s" % (GDAL_BIN_DIRECTORY,SYS_REF,ZOOM_START,ZOOM_END,tiffFile,target)
 os.system(cmd)
 print "Tiles created successfully"
+sys.stdout.flush()
+
 
 tiles_time = time.time() - mosaic_time - start_time
 
 # borrado de temporales    
 if remove_tmp:       
     shutil.rmtree(tmp)
+    
+if remove_output_XML:
+    print "Removing XML files"
+    sys.stdout.flush()
+    for root, dir, files in os.walk(target):
+        for file in files:
+            fileName, fileExtension = os.path.splitext(file)
+            if fileExtension ==".xml":
+                os.unlink(os.path.join(root,file))
+                
+                
+    print "XML files removed successfully"
+    sys.stdout.flush()
+
     
 elapsed_time = time.time() - start_time
 
